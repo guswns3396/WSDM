14e4a61c6bba4e736da11fe2813da6b977685280
HyunJoon Yang <hyyang@ciscogpu01.ini.usc.edu>
2020-06-02 15:36:12 -0700

    module redefinition

--- run.py
+++ run.py
@@ -1,17 +1,18 @@
-import code.setup
+import code
 
 def main():
     msg = "Please select from the following options:"
     msg += "\n1) Setup Virtual Environment"
     msg += "\n2) Install Dependencies"
     msg += "\n3) Run Inference"
+    msg += "\n>"
 
     userinput = input(msg)
 
     if userinput == "1":
-        setup.setupVenv()
+        code.setup.setupVenv()
     elif userinput == "2":
-        setup.install()
+        code.setup.install()
     elif userinput == "3":
         pass
     else:
