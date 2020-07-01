a501b61054d96e4723433e60156a527f26c0969c
HyunJoon Yang <hyyang@ciscogpu01.ini.usc.edu>
2020-07-01 16:25:37 -0700

    test setupVenv

--- code/test_setup.py
+++ code/test_setup.py
@@ -4,8 +4,13 @@ import setup
 class TestSetup(unittest.TestCase):
 	def test_setupVenv_installsVenv(self):
 		path = setup.pathlib.Path(__file__).parent.parent.absolute()
-		path += "/env/bin/python3"
+		setup.os.chdir(path)
+		path = str(path.parent) + "/env/bin/python3"
+		print(path)
 
 		setup.setupVenv()
 
-		self.assertTrue(setup.os.path.isfile(path))
+		self.assertTrue(setup.os.path.exists(path))
+
+if __name__ == "__main__":
+	unittest.main()
