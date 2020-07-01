269e5c607fc23b06393317ae24b311c0bb66ca31
HyunJoon Yang <hyyang@ciscogpu01.ini.usc.edu>
2020-07-01 14:22:37 -0700

    modify cfgs

--- code/inference.py
+++ code/inference.py
@@ -2,18 +2,14 @@ import os
 import pathlib
 import shutil
 
-# ask user for path to their data
-def getPathToData() -> str:
+# display required BIDS format
+def displayBIDS() -> None:
 	print("Please make sure the data is organized in the following fashion")
 	print("myDataFolder/")
 	print("\tSubject1/")
 	print("\t\tanat/")
-	print("\t\t\tt1w.nii.gz")
-	print("\t\t\tflair.nii.gz")
-	path_d = input("Absolute path to data folder: ")
-	if path_d[-1] != "/":
-		path_d += "/"
-	return path_d
+	print("\t\t\tSubject1_T1W.nii.gz")
+	print("\t\t\tSubject1_FLAIR.nii.gz")
 
 # modify modelConfig.cfg
 def modifyMC() -> None:
@@ -49,71 +45,60 @@ def modifyTC() -> None:
 		print(content[:index1 + len("folderForOutput = ")] + "\"" + path_o + "\"" + "\n", file=file)
 		print(content[index2:], file=file)
 
+# get list of subjects
+def getSubjects(path: "path to directory of data") -> list:
+	subjects = []
+	dirlist = os.listdir(str(path))
+	for file in dirlist:
+		if os.path.isdir(str(path) + "/" + file):
+			subjects.append(file)
+	subjects.sort()
+	return subjects
+
 # modify names of prediction
-def modifyPred(subs: "list of subjects") -> None:
+def modifyPred(subjects: "list of subjects") -> None:
 	# get path to pred cfg
-	path_pd = os.getcwd() + "/config/test/testNamesOfPredictions.cfg"
+	path = pathlib.Path(__file__).parent.parent.absolute()
+	path_pd = str(path) + "/deepmedic/inference_model/config/test/testNamesOfPredictions.cfg"
 	# open cfg file for writing
-	cfgFile = open(path_pd, "w")
-	# write for each subject:
-	for sub in subs:
-		# get name of prediction
-		name = "pred_" + sub + ".nii.gz"
-		# write to cfg
-		print(name, file=cfgFile)
+	with open(path_pd,'w') as file:
+		# write for each subject
+		for subject in subjects:
+			name = subject + "_WMH.nii.gz"
+			print(name, file=file)
 
 # modify test channels & prediction
-def modifyCfgs() -> None:
-	# ask for path to data
-	path_d = getPathToData()
+def modifyCfgs(path: "path to directory of data") -> None:
+	# get path to channel cfg
+	path_cfg = pathlib.Path(__file__).parent.parent.absolute()
 	# channel cfgs
-	path_flair = os.getcwd() + "/config/test/test_flair.cfg"
-	path_t1w = os.getcwd() + "/config/test/test_t1w.cfg"
+	path_flair = str(path_cfg) + "/deepmedic/inference_model/config/test/test_flair.cfg"
+	path_t1w = str(path_cfg) + "/deepmedic/inference_model/config/test/test_t1w.cfg"
 	path_cfgs = [path_flair, path_t1w]
-	# modalities
-	modal = ["flair", "t1w"]
-	# get subjects
-	subjects_all = os.listdir(path_d)
-	subjects_all.sort()
-	# get subjects for each modality
-	subjects_mod = []
+	# get all subjects
+	subjects_all = getSubjects(path)
+	subjects = []
+	# make sure t1w and flair images are both present
+	for subject in subjects_all:
+		image_path = str(path) + "/" + subject + "/anat"
+		hasT1W = os.path.isfile(image_path + "/" + subject + "_T1W.nii.gz")
+		hasFLAIR = os.path.isfile(image_path + "/" + subject + "_FLAIR.nii.gz")
+		if hasT1W and hasFLAIR:
+			subjects.append(subject)
+		else:
+			print("Missing required image(s) for subject: " + subject)
+	# modify channel cfgs
+	modalities = ["FLAIR", "T1W"]
 	for i in range(0, len(path_cfgs)):
-		subs = []
-		for sub in subjects_all:
-			# get path of image for subject
-			path = path_d + sub + "/" + modal[i] + ".nii.gz"
-			# make sure file exists
-			if os.path.isfile(path):
-				subs.append(sub)
-		subjects_mod.append(subs)
-	# check if subjects match for all modalities
-	for mod in subjects_mod:
-		if subjects_mod[0] != mod:
-			## TODO: show which image doesn't match
-			raise ValueError("Images for all modalities must match!")
-	print("Images for all modalities matched!")
-	# extract path to image according to modality
-	for i in range(0, len(path_cfgs)):
-		# open cfg file for writing
-		cfgFile = open(path_cfgs[i], "w")
-		# write for each subject
-		for sub in subjects_mod[0]:
-			# get path of image for subject
-			path = path_d + sub + "/" + modal[i] + ".nii.gz"
-			# write to cfg
-			print(path, file=cfgFile)
-	# modify prediction cfg
-	modifyPred(subjects_mod[0])
-
-# ask user for path to deepmedic folder
-def getPathToDM() -> str:
-	path_d = input("Absolute path to deepmedic folder: ")
-	if path_d[-1] != "/":
-		path_d += "/"
-	return path_d
+		with open(path_cfgs[i],'w') as file:
+			for subject in subjects:
+				img = str(path) + "/" + subject + "/anat/" + subject + "_" + modalities[i] + ".nii.gz"
+				print(img, file=file)
+	# modify pred cfg
+	modifyPred(subjects)
 
 # run inference
-def runInf() -> None:
+def runInf(path_dm: "path to deepmedic", path_data: "path to data") -> None:
 	# prepare different parts of command
 	path_d = getPathToDM()
 	run = path_d + "deepMedicRun"
@@ -127,10 +112,3 @@ def runInf() -> None:
 	os.system(run + " " +  model + " " + test + " " +  load + " " + dev + " >& out.txt &")
 	print("Running inference. You can check the progress using 'cat out.txt | less'")
 	print("Or 'ps aux | grep -i myUserName' to check if the process is still running")
-
-def main():
-	modifyMC()
-	modifyTC()
-	modifyCfgs()
-	print("Successfully modified channel & prediction configuration files!")
-	runInf()
--- run.py
+++ run.py
@@ -1,21 +1,19 @@
+import sys
 from code import setup
+from code import inference
 
-def main():
-    msg = "Please select from the following options:"
-    msg += "\n1) Setup Virtual Environment"
-    msg += "\n2) Install Dependencies"
-    msg += "\n3) Run Inference"
-    msg += "\n>"
-
-    userinput = input(msg)
-
-    if userinput == "1":
-        setup.setupVenv()
-    elif userinput == "2":
-        setup.install()
-    elif userinput == "3":
-        pass
-    else:
-        "Invalid option"
-
-main()
+if __name__ == '__main__':
+	if len(sys.argv) == 1:
+		print("Not enough command line arguments")
+	else:
+		# setup venv
+		if sys.argv[1] == '1' and len(sys.argv) == 2:
+			setup.setupVenv()
+		# install dependencies
+		elif sys.argv[1] == '2' and len(sys.argv) == 2:
+			setup.install()
+		# run inference
+		elif sys.argv[1] == '3' and len(sys.argv) == 3:
+			pass
+		else:
+			print("Invalid command line argument(s)")
