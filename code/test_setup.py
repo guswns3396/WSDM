a28711aaefd9bd85b566d46720d4c3f94d02a2b8
HyunJoon Yang <hyyang@ciscogpu01.ini.usc.edu>
2020-07-01 16:55:35 -0700

    test setupVenv

--- code/test_setup.py
+++ code/test_setup.py
@@ -1,16 +1,26 @@
 import unittest
+import os
+import pathlib
 import setup
 
 class TestSetup(unittest.TestCase):
+	def setUp(self):
+		pass
+	def tearDown(self):
+		if setup.os.path.exists("env"):
+			setup.os.system("rm -rf env/")
 	def test_setupVenv_installsVenv(self):
-		path = setup.pathlib.Path(__file__).parent.parent.absolute()
-		setup.os.chdir(path)
-		path = str(path.parent) + "/env/bin/python3"
+		path = pathlib.Path(__file__).parent.absolute()
+		path = path.parent.absolute()
+		print(path)
+		os.chdir(path)
+		print("Current Directory: " + str(os.getcwd()))
+		path = str(path) + "/env/bin/python3"
 		print(path)
 
 		setup.setupVenv()
 
-		self.assertTrue(setup.os.path.exists(path))
+		self.assertTrue(os.path.exists(path))
 
 if __name__ == "__main__":
 	unittest.main()
