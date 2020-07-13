bcb9178cb683f9bb29990f8f8eae262435e32c1d
HyunJoon Yang <hyyang@ciscogpu01.ini.usc.edu>
2020-07-13 15:43:18 -0700

    test venv installation

--- code/test_setup.py
+++ code/test_setup.py
@@ -7,20 +7,42 @@ class TestSetup(unittest.TestCase):
 	def setUp(self):
 		pass
 	def tearDown(self):
-		if setup.os.path.exists("env"):
-			setup.os.system("rm -rf env/")
+		# path to package directory
+		print("TEARING DOWN")
+		# get cwd
+		cwd = os.getcwd()
+		print("Current working directory:", cwd)
+		# package_path => dm_package/
+		package_path = pathlib.Path(__file__).parent.absolute().parent.absolute()
+		os.chdir(package_path)
+		print("Changed working directory to:", os.getcwd())
+		if os.path.exists("env"):
+			print("VENV FOUND - REMOVING...")
+			os.system("rm -rf env/")
+			print("REMOVED")
+		else:
+			print("VENV NOT FOUND")
+		os.chdir(cwd)
+		print("Changed working directory to:", os.getcwd())
 	def test_setupVenv_installsVenv(self):
+		print("SETTING UP TO INSTALL VENV")
+		path = pathlib.Path(__file__).absolute()
+		print("Absolute path to current file:", path)
 		path = pathlib.Path(__file__).parent.absolute()
+		print("Absolute path to directory of file:", path)
 		path = path.parent.absolute()
-		print(path)
-		os.chdir(path)
-		print("Current Directory: " + str(os.getcwd()))
+		print("Absolute path to directory containing directory of file:", path)
 		path = str(path) + "/env/bin/python3"
-		print(path)
+		print("Path to venv python:", path)
 
+		print("INSTALLING VENV")
+		print("-"*20)
 		setup.setupVenv()
+		print("-"*20)
 
 		self.assertTrue(os.path.exists(path))
+	def test_install_installsDependencies(self):
+		pass
 
 if __name__ == "__main__":
 	unittest.main()
