00d905ae6f56b482a3f2db704f2624e49740f736
HyunJoon Yang <hyyang@ciscogpu01.ini.usc.edu>
2020-07-14 15:41:41 -0700

    tested modifyMC()

--- code/test_inference.py
+++ code/test_inference.py
@@ -32,6 +32,22 @@ class TestSetup(unittest.TestCase):
 		setup.os.chdir(cwd)
 		print("DONE")
 
+	def tearDown(self):
+		print("TEARING DOWN")
+		code_path = setup.pathlib.Path(__file__).parent.absolute()
+		package_path = str(code_path.parent.absolute())
+		cmd = "git checkout -- "
+		config_path = "/deepmedic/inference_model/config"
+		cmd += package_path + config_path
+
+		setup.os.system(cmd + "/model/modelConfig.cfg")
+		setup.os.system(cmd + "/test/testConfig.cfg")
+		setup.os.system(cmd + "/test/testNamesOfPredictions.cfg")
+		setup.os.system(cmd + "/test/test_flair.cfg")
+		setup.os.system(cmd + "/test/test_t1w.cfg")
+
+		print("DONE")
+
 	def test_displayBIDS_displaysText(self):
 		print("ARRANGE - DISPLAYBIDS")
 		print("-"*20)
@@ -41,11 +57,35 @@ class TestSetup(unittest.TestCase):
 		with patch("sys.stdout", new=StringIO()) as fake_out:
 			inference.displayBIDS()
 			output = fake_out.getvalue()
+
 		print("ASSERT - DISPLAYBIDS")
 		expected = "Please make sure the data is organized in the following fashion\n"
 		expected += "myDataFolder/\n" + "\tSubject1/\n" + "\t\tanat/\n"
 		expected += "\t\t\tSubject1_T1W.nii.gz\n" + "\t\t\tSubject1_FLAIR.nii.gz\n"
 		self.assertEqual(expected, output)
+
+	def test_modifyMC_modifiesOutputFolder(self):
+		print("ARRANGE - MODIFYMC")
+		print("-"*20)
+		package_path = setup.pathlib.Path(__file__).parent.absolute().parent.absolute()
+		path_mc = str(package_path) + "/deepmedic/inference_model/config/model/modelConfig.cfg"
+		print("Path to MC:", path_mc)
+
+		print("ACT - MODIFYMC")
+		print("-"*20)
+		inference.modifyMC()
+
+		print("ASSERT - MODIFYMC")
+		print("-"*20)
+		old = "\"/ifs/loni/faculty/farshid/img/members/jack/dmtest/deepmedic/trial_3_pipeline/output/\""
+		with open(path_mc,'r') as f:
+			content = f.read()
+		index1 = content.find("folderForOutput = ")
+		index2 = content.find("#  ================ MODEL PARAMETERS")
+		new = content[index1 + len("folderForOutput = "):index2]
+		print(old)
+		print(new)
+		self.assertNotEqual(old, new)
 
 if __name__ == "__main__":
 	unittest.main()
