3de1a1963204860f3d2cd7f698e3602df99a1474
HyunJoon Yang <hyyang@ciscogpu01.ini.usc.edu>
2020-07-14 12:43:21 -0700

    setting up venv & installing dependencies run together in run.py

--- run.py
+++ run.py
@@ -6,16 +6,14 @@ if __name__ == '__main__':
 	if len(sys.argv) == 1:
 		print("Not enough command line arguments")
 	else:
-		# setup venv
+		# setup venv & install dependencies
 		if sys.argv[1] == '1' and len(sys.argv) == 2:
 			setup.setupVenv()
 			setup.addActivator()
-		# install dependencies
-		elif sys.argv[1] == '2' and len(sys.argv) == 2:
 			setup.activate()
 			setup.install()
 		# run inference
-		elif sys.argv[1] == '3' and len(sys.argv) == 3:
+		elif sys.argv[1] == '2' and len(sys.argv) == 3:
 			pass
 		else:
 			print("Invalid command line argument(s)")
