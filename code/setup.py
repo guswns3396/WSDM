1f7a6fbd1d9c107b8dae2e38fe6413e597244d67
HyunJoon Yang <hyyang@ciscogpu01.ini.usc.edu>
2020-07-13 17:21:50 -0700

    added code for venv activation

--- code/setup.py
+++ code/setup.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import shutil
 
 def setupVenv():
     # create virtual environment for python
@@ -26,17 +27,44 @@ def setupVenv():
 
     return
 
+def addActivator():
+    print("ADDING ACTIVATOR FOR VENV")
+    # get path to code folder
+    code_path = pathlib.Path(__file__).parent.absolute()
+    print("Code directory:", code_path)
+    # get path to package folder
+    package_path = code_path.parent.absolute()
+    print("Package directory:", package_path)
+    # get path to activate_this file
+    file_path = str(code_path) + "/activate_this.py"
+    print("File path:", file_path)
+    # get path to bin folder
+    bin_path = str(package_path + "/env/bin/")
+    print("env/bin directory:", bin_path)
+    # add activate_this.py
+    shutil.copyfile(file_path, bin_path)
+    print("DONE")
+
 def install():
     # install packages
     # -----------------
     print("INSTALLING DEPENDENCIES")
+    # get cwd
+    cwd = os.getcwd()
+    print("Current directory:", cwd)
     # get path to package folder
     path = pathlib.Path(__file__).parent.parent.absolute()
+    print("Package directory:", path)
     # change directory to package folder then install
+    print("Changing directory to:", path)
     os.chdir(path)
+    print("Installin pip and dependencies")
     os.system("pip install --upgrade pip")
     # install dependencies
     os.system("pip install -r requirements.txt")
+    # change directory back
+    print("Changing directory to:", cwd)
+    os.chdir(cwd)
     print("DONE")
 
     return
