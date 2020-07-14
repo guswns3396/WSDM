31f688b72c52be1e612eb5de613d51bb24af6a1e
HyunJoon Yang <hyyang@ciscogpu01.ini.usc.edu>
2020-07-14 11:18:08 -0700

    no duplicate imports

--- code/test_setup.py
+++ code/test_setup.py
@@ -1,6 +1,4 @@
 import unittest
-import os
-import pathlib
 import setup
 
 class TestSetup(unittest.TestCase):
@@ -8,7 +6,7 @@ class TestSetup(unittest.TestCase):
 		print("SETTING UP")
 	
 		# print paths for verification
-		file_path = pathlib.Path(__file__).absolute()
+		file_path = setup.pathlib.Path(__file__).absolute()
 		print("Absolute path to current file:", file_path)
 		code_path = file_path.parent.absolute()
 		print("Absolute path to directory of file:", code_path)
@@ -19,31 +17,31 @@ class TestSetup(unittest.TestCase):
 		print("TEARING DOWN")
 
 		# get cwd
-		cwd = os.getcwd()
+		cwd = setup.os.getcwd()
 		print("Current working directory:", cwd)
 
 		# change directory to package directory
 		# package_path => dm_package/
-		package_path = pathlib.Path(__file__).parent.absolute().parent.absolute()
-		os.chdir(package_path)
-		print("Changed working directory to:", os.getcwd())
+		package_path = setup.pathlib.Path(__file__).parent.absolute().parent.absolute()
+		setup.os.chdir(package_path)
+		print("Changed working directory to:", setup.os.getcwd())
 
 		# check if venv exists & remove
-		if os.path.exists("env"):
+		if setup.os.path.exists("env"):
 			print("VENV FOUND - REMOVING...")
-			os.system("rm -rf env/")
+			setup.os.system("rm -rf env/")
 			print("REMOVED")
 		else:
 			print("VENV NOT FOUND")
 
 		# change directory back
-		os.chdir(cwd)
-		print("Changed working directory to:", os.getcwd())
+		setup.os.chdir(cwd)
+		print("Changed working directory to:", setup.os.getcwd())
 
 	def test_setupVenv_installsVenv(self):
 		print("ARRANGE - SETUPVENV")
 		print("-"*20)
-		file_path = pathlib.Path(__file__).absolute()
+		file_path = setup.pathlib.Path(__file__).absolute()
 		code_path = file_path.parent.absolute()
 		package_path = code_path.parent.absolute()
 		venv_path = str(package_path) + "/env/bin/python3"
@@ -55,12 +53,12 @@ class TestSetup(unittest.TestCase):
 
 		print("ASSERT - SETUPVENV")
 		print("-"*20)
-		self.assertTrue(os.path.exists(venv_path))
+		self.assertTrue(setup.os.path.exists(venv_path))
 
 	def test_addActivator_copiesFile(self):
 		print("ARRANGE - ADDACTIVATOR")
 		print("-"*20)
-		package_path = pathlib.Path(__file__).parent.absolute().parent.absolute()
+		package_path = setup.pathlib.Path(__file__).parent.absolute().parent.absolute()
 		print("Path to package:", package_path)
 		setup.setupVenv()
 
@@ -70,20 +68,17 @@ class TestSetup(unittest.TestCase):
 
 		print("ASSERT - ADDACTIVATOR")
 		print("-"*20)
-		self.assertTrue(os.path.exists(str(package_path) + "/env/bin/activate_this.py"))
+		self.assertTrue(setup.os.path.exists(str(package_path) + "/env/bin/activate_this.py"))
 '''
 	def test_install_installsDependencies(self):
-		# setup & activate venv
-		print("SETTING UP TO INSTALL DEPENDENCIES")
+		print("ARRANGE - INSTALL")
+		print("-"*20)
 		setup.setupVenv()
-		
+		setup.addActivator()
 
-		print("INSTALLING DEPENDENCIES")
+		print("ACT - INSTALL")
 		print("-"*20)
 		setup.install()
-		print("-"*20)
 '''
-		
-
 if __name__ == "__main__":
 	unittest.main()
