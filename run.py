a89717cb9ab13a98f1958b77e13af33f5d66ad0d
HyunJoon Yang <hyyang@ciscogpu01.ini.usc.edu>
2020-07-21 15:13:00 -0700

    activation no longer part of run.py

--- run.py
+++ run.py
@@ -9,14 +9,15 @@ if __name__ == '__main__':
 		# setup venv & install dependencies
 		if sys.argv[1] == '1' and len(sys.argv) == 2:
 			setup.setupVenv()
-			setup.addActivator()
-			setup.activate()
+			# setup.addActivator()
+			# setup.activate()
+		elif sys.argv[1] == '2' and len(sys.argv) == 2:
 			setup.install()
 		# run inference
-		elif sys.argv[1] == '2' and len(sys.argv) == 4:
+		elif sys.argv[1] == '3' and len(sys.argv) == 4:
 			inference.displayBIDS()
 
-			setup.activate()
+			# setup.activate()
 
 			inference.modifyMC()
 			inference.modifyTC()
